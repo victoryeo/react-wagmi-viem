f7de3d4e4570423f72a79e8bd9f024c3abb1e8fa
yeosiowvic <victoryeo1974@gmail.com>
2024-02-16 20:44:12 +0800

    add read smart contract

--- src/Dapp.tsx
+++ src/Dapp.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useWeb3Modal } from "@web3modal/react";
-import { useAccount, useDisconnect } from "wagmi";
+import { useAccount, useDisconnect, useReadContract } from "wagmi";
+import { greeter_abi } from "./greeter_abi.json";
 
 const Dapp = () => {
   const { open } = useWeb3Modal();
   const { address, isConnected } = useAccount();
   const { disconnect } = useDisconnect();
+
+  const { data: currentGreet = "", refetch: reFetchGreeter }: any =
+    useReadContract({
+      abi: greeter_abi,
+      address: "0xd24FcAedcc75dF6d9AE8581B9836e9781AE89fE8",
+      functionName: "greet",
+    });
 
   return (
     <div>
