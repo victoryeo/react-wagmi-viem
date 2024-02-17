2fdf24d78d2e644142205dc726ec284bac439c80
yeosiowvic <victoryeo1974@gmail.com>
2024-02-17 15:55:46 +0800

    add wagmi wallet connectors

--- src/config.tsx
+++ src/config.tsx
@@ -1,9 +1,19 @@
 import { http, createConfig } from "wagmi";
 import { bscTestnet } from "wagmi/chains";
+import { injected } from 'wagmi/connectors' 
+import { walletConnect } from 'wagmi/connectors'
+import { metaMask } from "wagmi/connectors";
 
 export const wagmiConfig = createConfig({
   chains: [bscTestnet],
   transports: {
     [bscTestnet.id]: http(),
   },
+  connectors: [
+    injected(), 
+    metaMask(),
+    walletConnect({
+      projectId: '3fcc6bba6f1de962d911bb5b5c3dba68',
+    }),
+  ],
 });
